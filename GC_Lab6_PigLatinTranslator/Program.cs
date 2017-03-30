4c91fcc2be4cdb9b66cda689c4faaa3658ccb29b
Pete Rossman <pete.rossman@gmail.com>
2017-03-30 15:39:10 -0400

    First commit

---
+++ GC_Lab6_PigLatinTranslator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Pete Rossman
+//Grand Circus .NET Bootcamp Lab 6
+//March 30, 2017
+
+namespace GC_Lab6_PigLatinTranslator
+{
+    //This program takes a word in English and translates it to Pig Latin.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            Console.WriteLine("\nEnter a word to be translated: ");
+            string word = Console.ReadLine();
+            word = word.ToLower();
+            char firstLetter = word[0];
+            bool isVowel = "aeiouAEIOU".IndexOf(firstLetter) >= 0;
+            if (isVowel == true)
+            {
+                Console.WriteLine(word + "way");
+            }
+            else
+            {
+                string newWord = word.Remove(0, 1);
+                Console.WriteLine(newWord + firstLetter + "way");
+            }
+            Console.ReadLine();
+        }
+    }
+}
