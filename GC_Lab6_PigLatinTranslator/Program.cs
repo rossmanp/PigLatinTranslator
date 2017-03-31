5f6495533dd4a7ca408b288711dbd1319160fe13
Pete Rossman <pete.rossman@gmail.com>
2017-03-31 13:59:27 -0400

    Add comments

--- GC_Lab6_PigLatinTranslator/Program.cs
+++ GC_Lab6_PigLatinTranslator/Program.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace GC_Lab6_PigLatinTranslator
 {
-    //This program takes a word in English and translates it to Pig Latin.
+    //This program takes a word or sentence in English and translates it to Pig Latin.
 
     class Program
     {
@@ -20,8 +20,15 @@ namespace GC_Lab6_PigLatinTranslator
             bool run = true;
             char[] delimiterChars = { ' ' };           
             string word = "";
-            char[] SpecialChars = "@#$%^&*()".ToCharArray();
+
+            //This array is used to detect if a word contains symbols.
+            //If a word does contain one of the symbols in the array, it is not translated.
+            char[] symbols = "@#$%^&*()".ToCharArray();
+
             Console.WriteLine("Welcome to the Pig Latin Translator!");
+
+            //This loop is run until the user inputs a character, word, or sentence.
+
             while (run)
             {
                 Console.WriteLine("\nEnter a word or sentence to be translated: ");
@@ -35,14 +42,20 @@ namespace GC_Lab6_PigLatinTranslator
                     run = false;
                 }
             }
+
+            //The input is converted to lowercase and split into words.
             word = word.ToLower();
             string[] words = word.Split(delimiterChars);
             Console.Write("The Pig Latin translation is: ");
+
+            //The foreach loop detects if each word starts with a vowel or consonant,
+            //as well as if there are any numbers or symbols in the word.
+
             foreach (string s in words)
             {
                 char firstLetter = s[0];
                 bool isVowel = "aeiouAEIOU".IndexOf(firstLetter) >= 0;
-                int indexOf = s.IndexOfAny(SpecialChars);
+                int indexOf = s.IndexOfAny(symbols);
                 if (indexOf > -1 || s.Any(char.IsDigit))
                 {
                     Console.Write(s + " ");
