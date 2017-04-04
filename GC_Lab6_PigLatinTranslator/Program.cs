211013b0b0bdf82a8856c565e23886c494de9c33
Pete Rossman <pete.rossman@gmail.com>
2017-04-04 10:07:54 -0400

    Continue method implemented to give user ability to repeat translator as desired

--- GC_Lab6_PigLatinTranslator/Program.cs
+++ GC_Lab6_PigLatinTranslator/Program.cs
@@ -19,8 +19,9 @@ namespace GC_Lab6_PigLatinTranslator
         static void Main(string[] args)
         {
             bool run = true;
-            char[] delimiterChars = { ' ' };           
+            char[] delimiterChars = { ' ' };
             string word = "";
+            bool programRun = true;
 
             //This array is used to detect if a word contains symbols.
             //If a word does contain one of the symbols in the array, it is not translated.
@@ -30,119 +31,123 @@ namespace GC_Lab6_PigLatinTranslator
             Console.WriteLine("Welcome to the Pig Latin Translator!");
 
             //This loop is run until the user inputs a character, word, or sentence.
-
-            while (run)
+            while (programRun)
             {
-                Console.WriteLine("\nEnter a word or sentence to be translated: ");
-                word = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(word))
+                while (run)
                 {
-
-                    Console.WriteLine("You did not input anything! Please try again.");
-                }
-                else
-                {
-                    run = false;
-                }
-            }
-
-            //The input is split at white spaces into words. 
-                       
-            string[] words = word.Split(delimiterChars);
-            Console.Write("The Pig Latin translation is: ");
-
-            //The foreach loop detects if each word starts with a vowel or consonant,
-            //as well as if there are any numbers or symbols in the word.
-
-            foreach (string s in words)
-            {
-                string vowels = "aeiouAEIOU";               
-                int firstVowel = 0;              
-                foreach (char c in s)
-                {                   
-                    if (vowels.Contains(c))
+                    Console.WriteLine("\nEnter a word or sentence to be translated: ");
+                    word = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(word))
                     {
-                        firstVowel = s.IndexOf(c);                       
-                    }
-                    if (firstVowel > 0)
-                    {
-                        break;
-                    }
-                }
-                
-                int indexOf = s.IndexOfAny(symbols);
-                string way = "way";
 
-                // If a word contains a symbol or a number, the word is not translated.
-
-                if (indexOf > -1 || s.Any(char.IsDigit))
-                {
-                    Console.Write(s + " ");
-                }
-                
-                //If the word starts with a vowel, "way" is appended to the word's end.
-                //If a word ends with a punctuation mark, that mark is kept.
-                                                                                                     
-                else if (firstVowel == 0)
-                {
-                    if (IsAllUpper(s))
-                    {
-                        way = "WAY";
-                    }
-                    if (Char.IsPunctuation(s[s.Length - 1]))
-                    {
-                        Console.Write(s.Substring(0, s.Length - 1) + way + s[s.Length - 1] + " ");
+                        Console.WriteLine("You did not input anything! Please try again.");
                     }
                     else
                     {
-                        Console.Write(s + way + " ");
+                        run = false;
                     }
                 }
 
-                //Translate a word beginning with a consonant to Pig Latin
-                //by cutting the start of the word to the first vowel,  
-                //pasting that part at the end, and then "way" is added.
-                //Any punctuation used at the end is maintained.
+                //The input is split at white spaces into words. 
 
-                else
+                string[] words = word.Split(delimiterChars);
+                Console.Write("The Pig Latin translation is: ");
+
+                //The foreach loop detects if each word starts with a vowel or consonant,
+                //as well as if there are any numbers or symbols in the word.
+
+                foreach (string s in words)
                 {
-                    string firstPart = s.Substring(0, firstVowel);
-                    firstPart = firstPart.ToLower();
-                    string newWord = s.Remove(0, firstVowel);
-                    way = "ay";
-                    //This conditional maintains proper capitalization for the title case.
-
-                    if (Char.IsUpper(s[0]) && !IsAllUpper(s))
-                    {                      
-                        newWord = Char.ToUpper(newWord[0]) + newWord.Substring(1, newWord.Length - 1);                                              
+                    string vowels = "aeiouAEIOU";
+                    int firstVowel = 0;
+                    foreach (char c in s)
+                    {
+                        if (vowels.Contains(c))
+                        {
+                            firstVowel = s.IndexOf(c);
+                        }
+                        if (firstVowel > 0)
+                        {
+                            break;
+                        }
                     }
 
-                    //This conditional changes "way" to "WAY" if each letter in the word is capitalized.
+                    int indexOf = s.IndexOfAny(symbols);
+                    string way = "way";
 
-                    if (IsAllUpper(s))
+                    // If a word contains a symbol or a number, the word is not translated.
+
+                    if (indexOf > -1 || s.Any(char.IsDigit))
                     {
-                        way = "AY";
-                        firstPart = firstPart.ToUpper();
+                        Console.Write(s + " ");
                     }
 
-                    //This statement makes sure "way" is added before any punctuation mark
-                    //at the end of a word.
+                    //If the word starts with a vowel, "way" is appended to the word's end.
+                    //If a word ends with a punctuation mark, that mark is kept.
 
-                    if (Char.IsPunctuation(newWord[newWord.Length - 1]))
+                    else if (firstVowel == 0)
                     {
-                        Console.Write(newWord.Substring(0, newWord.Length - 1) + 
-                        firstPart + way + newWord[newWord.Length - 1] + " ");
-                    } 
-                    
-                    //This last statement writes the word in Pig Latin.
-                                    
+                        if (IsAllUpper(s))
+                        {
+                            way = "WAY";
+                        }
+                        if (Char.IsPunctuation(s[s.Length - 1]))
+                        {
+                            Console.Write(s.Substring(0, s.Length - 1) + way + s[s.Length - 1] + " ");
+                        }
+                        else
+                        {
+                            Console.Write(s + way + " ");
+                        }
+                    }
+
+                    //Translate a word beginning with a consonant to Pig Latin
+                    //by cutting the start of the word to the first vowel,  
+                    //pasting that part at the end, and then "way" is added.
+                    //Any punctuation used at the end is maintained.
+
                     else
                     {
-                        Console.Write(newWord + firstPart + way + " ");
+                        string firstPart = s.Substring(0, firstVowel);
+                        firstPart = firstPart.ToLower();
+                        string newWord = s.Remove(0, firstVowel);
+                        way = "ay";
+                        //This conditional maintains proper capitalization for the title case.
+
+                        if (Char.IsUpper(s[0]) && !IsAllUpper(s))
+                        {
+                            newWord = Char.ToUpper(newWord[0]) + newWord.Substring(1, newWord.Length - 1);
+                        }
+
+                        //This conditional changes "way" to "WAY" if each letter in the word is capitalized.
+
+                        if (IsAllUpper(s))
+                        {
+                            way = "AY";
+                            firstPart = firstPart.ToUpper();
+                        }
+
+                        //This statement makes sure "way" is added before any punctuation mark
+                        //at the end of a word.
+
+                        if (Char.IsPunctuation(newWord[newWord.Length - 1]))
+                        {
+                            Console.Write(newWord.Substring(0, newWord.Length - 1) +
+                            firstPart + way + newWord[newWord.Length - 1] + " ");
+                        }
+
+                        //This last statement writes the word in Pig Latin.
+
+                        else
+                        {
+                            Console.Write(newWord + firstPart + way + " ");
+                        }
                     }
                 }
+                Console.WriteLine("\nDo you want to enter another word or sentence (y/n): ");
+                programRun = Continue();
+                run = true;             
             }
-            Console.ReadLine();
         }
 
         // This method detects if a word consists of all uppercase characters.
@@ -159,7 +164,31 @@ namespace GC_Lab6_PigLatinTranslator
 
             return true;
         }
-    }
 
-   
+        public static Boolean Continue()
+        {
+            string input = Console.ReadLine();
+            Boolean run = true;
+            input = input.ToLower();
+
+            if (input == "n")
+            {
+                Console.WriteLine("\nGoodbye!");
+                run = false;
+                Console.ReadLine();
+            }
+            else if (input == "y")
+            {
+                run = true;
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, I didn't understand your input. Let's try that again!");
+                run = Continue();
+            }
+
+            return run;
+        }
+
+    }   
 }
